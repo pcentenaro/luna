13f3ac484bf8f939b6de60ede34d7190b7b81af0
Void <phc7blu@gmail.com>
2026-08-17 14:02:30 -0300

    removes debug guilds parameter from bot initialization

--- src/config.py
+++ src/config.py
@@ -12,8 +12,7 @@ intents.message_content = True
 bot = commands.Bot(
     command_prefix="!",
     help_command=None,
-    intents=intents,
-    debug_guilds=[os.getenv("DISCORD_GUILD_ID")],
+    intents=intents
 )
 
 startgg_api_key = os.getenv("STARTGG_API_KEY")

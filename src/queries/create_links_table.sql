3c147c8ba45e632977b6fbd5f3ed57e5a104e916
Lao <eduardo.cornejo.balcazar@gmail.com>
2026-09-06 14:19:19 -0600

    fix: include PGRS columns in links schema

--- src/queries/create_links_table.sql
+++ src/queries/create_links_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS links (
     discord_user_id TEXT UNIQUE,
     startgg_gamer_tag TEXT NOT NULL,
     startgg_prefix TEXT,
+    pgrs_player_name TEXT,
+    pgrs_player_id TEXT,
     updated_at TEXT NOT NULL
 )
